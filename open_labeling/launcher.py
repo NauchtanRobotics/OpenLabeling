12c3a01dd60559a3ef1aad66d456e2ce31e0cf23
David <davidmacnaugh@gmail.com>
2021-11-26 08:21:03 +1000

    Cleanup file selector

--- open_labeling/launcher.py
+++ open_labeling/launcher.py
@@ -19,26 +19,12 @@ def main():
             sg.In(size=(40, 1), enable_events=True, key="-FOLDER-"),
             sg.FolderBrowse(),
         ],
-        # [
-        #     sg.Listbox(
-        #         values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
-        #     )
-        # ],
     ]
-
-    # For now will only show the name of the file that was chosen
-    # image_viewer_column = [
-    #     [sg.Text("Choose an image from list on left:")],
-    #     [sg.Text(size=(40, 1), key="-TOUT-")],
-    #     [sg.Image(key="-IMAGE-")],
-    # ]
 
     # ----- Full layout -----
     layout = [
         [
             sg.Column(file_list_column),
-            # sg.VSeperator(),
-            # sg.Column(image_viewer_column),
         ]
     ]
 
@@ -64,31 +50,6 @@ def main():
             open_labeling_thread.start()
 
             window.close()
-            # try:
-            #     # Get list of files in folder
-            #     file_list = os.listdir(folder)
-            # except:
-            #     file_list = []
-
-            # fnames = [
-            #     f
-            #     for f in file_list
-            #     if os.path.isfile(os.path.join(folder, f))
-            #     and f.lower().endswith((".png", ".PNG", ".jpg", ".JPG"))
-            # ]
-            # window["-FILE LIST-"].update(fnames)
-
-        # elif event == "-FILE LIST-":  # A file was chosen from the listbox
-        #     try:
-        #         filename = os.path.join(
-        #             values["-FOLDER-"], values["-FILE LIST-"][0]
-        #         )
-        #         window["-TOUT-"].update(filename)
-        #         window["-IMAGE-"].update(filename=filename)
-        #     except:
-        #         pass
-
-    # window.close()
 
 
 if __name__ == '__main__':
