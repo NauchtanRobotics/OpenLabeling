4acd2bbd94e5a2cd841d1f10470db067dd872224
david <davidmacnaugh@gmail.com>
2024-07-04 12:19:42 +1000

    Reduced the minimum number of images to constitute a datasource folder.

--- open_labeling/common.py
+++ open_labeling/common.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 
 
-def check_if_folder_contains_sufficient_images(input_dir: Path, threshold: int = 3):
+def check_if_folder_contains_sufficient_images(input_dir: Path, threshold: int = 2):
     """
     Raises a runtime error which provides a useful tip to GUI user if
     the selected folder does not contain sufficient images.
     """
     if not input_dir.is_dir():
-        raise RuntimeError("")
+        raise RuntimeError("The root-folder provided does not exist or not a folder.")
+
     image_file_paths = list(input_dir.iterdir())
     image_file_paths = [
         image_file_path for image_file_path in image_file_paths
